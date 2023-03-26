5010b419043b4f84935cb0e4354784e0d20ccf7d
TuichD <midtt@mail.ru>
2023-03-26 14:05:50 +0300

    Функции начало

---
+++ Examples/Example002_Hello user/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ввеидте ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
+
+
+
+
+
+
+
---
+++ Examples/Example005_Like/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+
+  Console.WriteLine("Ура , это же МАША!");
+
+}
+else
+{
+  Console.WriteLine("Привет,");
+  Console.WriteLine(username);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
