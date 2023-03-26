b28be98b2b8740c6a94407008192932e9dbeb36e
TuichD <midtt@mail.ru>
2023-03-26 14:31:43 +0300

    индекс массива

--- Examples/Example008_ F1/Program.cs
+++ Examples/Example008_ F1/Program.cs
@@ -11,7 +11,7 @@ int a1 = 15;
 int b1 = 21;
 int c1 = 39;
 int a2 = 12;
-int b2 = 2311;
+int b2 = 23;
 int c2 = 33;
 int a3 = 13;
 int b3 = 23;
@@ -20,8 +20,8 @@ int c3 = 33;
 /*int max1 = Max(a1, b1, c1);
 int max2 = Max(a2, b2, c2);
 int max3 = Max(a3, b3, c3);*/
-int max = Max
-    (Max(a1, b1, c1), 
+int max = Max(
+    Max(a1, b1, c1), 
     Max(a2, b2, c2), 
     Max(a3, b3, c3));
 
